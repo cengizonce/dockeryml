6f5bca17b4247a031a0fed42ff06fd87d6e7bee5
cengizhanonce <cengizhan.once@testinium.com>
2022-11-17 02:36:23 +0300

    Second c

---
+++ src/test/java/OptionsManager.java
@@ -0,0 +1,32 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.firefox.FirefoxProfile;
+
+public class OptionsManager {
+
+    //Get Chrome Options
+    public static ChromeOptions getChromeOptions() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--start-maximized");
+        options.addArguments("--ignore-certificate-errors");
+        options.addArguments("--disable-popup-blocking");
+        //options.addArguments("--incognito");
+        return options;
+        /*ChromeDriverService service = new ChromeDriverService.Builder()
+                .usingAnyFreePort()
+                .build();
+        ChromeDriver driver = new ChromeDriver(service, options);*/
+    }
+
+    //Get Firefox Options
+    public static FirefoxOptions getFirefoxOptions () {
+        FirefoxOptions options = new FirefoxOptions();
+        FirefoxProfile profile = new FirefoxProfile();
+        //Accept Untrusted Certificates
+        //Set Firefox profile to capabilities
+        options.setCapability(FirefoxDriver.PROFILE, profile);
+        return options;
+    }
+}
